b5419627aa3847cdfe86b468a10863c44bb3a577
britishpop <drmakc@gmail.com>
2019-02-15 13:43:06 +0300

    создание списка в значении словаря. косяк в преобразовании строки в список. не знаю, как сделать

--- Coursera/storage.py
+++ Coursera/storage.py
@@ -8,12 +8,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--key")
 parser.add_argument("--value")
 args = parser.parse_args()
+
 key_value_store = {args.key:args.value}
+
 if args.value:
     try:
         with open(storage_path, "r") as f:
             storage = json.load(f)
             storage.update(key_value_store)
+            if args.key not in storage.keys():
+                storage.update(key_value_store)
+            elif args.key in storage.keys():
+                values_list = list(storage[args.key])
+                values_list.append(key_value_store[args.key])
+                storage[args.key] = values_list
         with open(storage_path, 'w') as f:
             json.dump(storage, f)
             print(args.key, args.value)
