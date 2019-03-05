dd608e7367e7ea8a171896e63aa26f638154e4a1
katyfrolova <ekaterina.frolova40@gmail.com>
2019-03-05 11:58:26 +0300

    server-client TCP chat for coursera

--- Coursera_1st_course/homeworks_6th_week/client.py
+++ Coursera_1st_course/homeworks_6th_week/client.py
@@ -1,84 +1,113 @@
-"""
-Это вспомогательный скрипт для тестирования сервера из задания на неделе 6.
-
-Для запуска скрипта на локальном компьютере разместите рядом файл client.py,
-где содержится код клиента, который открывается по прохождении задания
-недели 5.
-
-Сначала запускаете ваш сервер на адресе 127.0.0.1 и порту 8888, а затем
-запускаете этот скрипт.
-"""
-import sys
-from client import Client, ClientSocketError, ClientProtocolError
+import socket
+import time
 
 
-def run(host, port):
+class ClientError(Exception):
+    """Общий класс исключений клиента"""
+    pass
 
-    client1 = Client(host, port, timeout=5)
-    client2 = Client(host, port, timeout=5)
 
-    try:
-        client1.connection.sendall(b"malformed command test\n")
-        client1._read()
-        client2.connection.sendall(b"malformed command test\n")
-        client2._read()
-    except ClientSocketError as err:
-        print(f"Ошибка общения с сервером: {err.__class__}: {err}")
-        sys.exit(1)
-    except ClientProtocolError:
-        pass
-    else:
-        else:
-        print("Неверная команда, отправленная серверу, должна возвращать ошибку протокола")
-        sys.exit(1)
+class ClientSocketError(ClientError):
+    """Исключение, выбрасываемое клиентом при сетевой ошибке"""
+    pass
 
-    try:
-        client1.put("k1", 0.25, timestamp=1)
-        client2.put("k1", 2.156, timestamp=2)
-        client1.put("k1", 0.35, timestamp=3)
-        client2.put("k2", 30, timestamp=4)
-        client1.put("k2", 40, timestamp=5)
-        client1.put("k2", 40, timestamp=5)
-    except Exception as err:
-        print(f"Ошибка вызова client.put(...) {err.__class__}: {err}")
-        sys.exit(1)
 
-    expected_metrics = {
-        "k1": [(1, 0.25), (2, 2.156), (3, 0.35)],
-        "k2": [(4, 30.0), (5, 40.0)],
-    }
+class ClientProtocolError(ClientError):
+    """Исключение, выбрасываемое клиентом при ошибке протокола"""
+    pass
 
-    try:
-        metrics = client1.get("*")
-        if metrics != expected_metrics:
-            print(f"client.get('*') вернул неверный результат. Ожидается: {expected_metrics}. Получено: {metrics}")
-            sys.exit(1)
-    except Exception as err:
-        print(f"Ошибка вызова client.get('*') {err.__class__}: {err}")
-        sys.exit(1)
 
-    expected_metrics = {"k2": [(4, 30.0), (5, 40.0)]}
+class Client:
+    def __init__(self, host, port, timeout=None):
+        # класс инкапсулирует создание сокета
+        # создаем клиентский сокет, запоминаем объект socke.socket в self
+        self.host = host
+        self.port = port
+        try:
+            self.connection = socket.create_connection((host, port), timeout)
+        except socket.error as err:
+            raise ClientSocketError("error create connection", err)
 
-    try:
-        metrics = client2.get("k2")
-        if metrics != expected_metrics:
-            print(f"client.get('k2') вернул неверный результат. Ожидается: {expected_metrics}. Получено: {metrics}")
-            sys.exit(1)
-    except Exception as err:
-        print(f"Ошибка вызова client.get('k2') {err.__class__}: {err}")
-        sys.exit(1)
+    def _read(self):
+        """Метод для чтения ответа сервера"""
+        data = b""
+        # накапливаем буфер, пока не встретим "\n\n" в конце команды
+        while not data.endswith(b"\n\n"):
+            try:
+                data += self.connection.recv(1024)
+            except socket.error as err:
+                raise ClientSocketError("error recv data", err)
 
-    try:
-        result = client1.get("k3")
-        if result != {}:
-            print(f"Ошибка вызова метода get с ключом, который еще не был добавлен. Ожидается: пустой словарь. Получено: {result}")
-            sys.exit(1)
-    except Exception as err:
-        print(f"Ошибка вызова метода get с ключом, который еще не был добавлен: {err.__class__} {err}")
-        sys.exit(1)
+        # не забываем преобразовывать байты в объекты str для дальнейшей работы
+        decoded_data = data.decode()
 
-    print("Похоже, что все верно! Попробуйте отправить решение на проверку.")
+        status, payload = decoded_data.split("\n", 1)
+        payload = payload.strip()
+
+        # если получили ошибку - бросаем исключение ClientError
+        if status == "error":
+            raise ClientProtocolError(payload)
+
+        return payload
+
+    def put(self, key, value, timestamp=None):
+        timestamp = timestamp or int(time.time())
+
+        # отправляем запрос команды put
+        try:
+            self.connection.sendall(
+                f"put {key} {value} {timestamp}\n".encode()
+                )
+        except socket.error as err:
+            raise ClientSocketError("error send data", err)
+
+        # разбираем ответ
+        self._read()
+
+
+    def get(self, key):
+        # формируем и отправляем запрос команды get
+        try:
+          self.connection.sendall(
+              f"get {key}\n".encode()
+              )
+        except socket.error as err:
+             raise ClientSocketError("error send data", err)
+
+        # читаем ответ
+        payload = self._read()
+
+        data = {}
+        if payload == "":
+            return data
+
+        # разбираем ответ для команды get
+        for row in payload.split("\n"):
+            key, value, timestamp = row.split()
+            if key not in data:
+                data[key] = []
+            data[key].append((int(timestamp), float(value)))
+
+        return data
+
+    def close(self):
+         try:
+             self.connection.close()
+         except socket.error as err:
+             raise ClientSocketError("error close connection", err)
+
+def _main():
+  # проверка работы клиента
+    client = Client("127.0.0.1", 8888, timeout=5)
+    client.put("test", 0.5, timestamp=1)
+    client.put("test", 2.0, timestamp=2)
+    client.put("test", 0.5, timestamp=3)
+    client.put("load", 3, timestamp=4)
+    client.put("load", 4, timestamp=5)
+    print(client.get("*"))
+
+    client.close()
 
 
 if __name__ == "__main__":
-    run("127.0.0.1", 8888)
+    _main()
--- Coursera_1st_course/homeworks_6th_week/server.py
+++ Coursera_1st_course/homeworks_6th_week/server.py
@@ -1,8 +1,30 @@
 
 import asyncio
 
+def run_server(host, port):
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        ClientServerProtocol,
+        host, port
+    )
+
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
 
 class ClientServerProtocol(asyncio.Protocol):
+    def __init__(self):
+        self.data_dict = {}
+
     def connection_made(self, transport):
         self.transport = transport
 
@@ -15,23 +37,47 @@ class ClientServerProtocol(asyncio.Protocol):
             self.transport.write(resp.encode())
 
     def process_data(self, data):
-        pass
 
+        status, payload = data.split(" ", 1)
+        payload = payload.strip()
+        try:
+            if status == "get":
+                data_for_client = self.get(payload)
+                return data_for_client
 
+            if status == "put":
+                self.put(payload)
+                return "ok\n\n"
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(
-    ClientServerProtocol,
-    '127.0.0.1', 8181
-)
+        except:
+            return " "
 
-server = loop.run_until_complete(coro)
+    def get(self, payload):
+        data = "ok\n"
+        for key in self.data_dict:
+            if key == payload:
+                for metric_value in self.data_dict[key]:
+                    data += f"{payload} {metric_value[0]} {metric_value[1]}\n"
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+            if payload == "*":
+                for metric_value in self.data_dict[key]:
+                    data += f"{key} {metric_value[0]} {metric_value[1]}\n"
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+            else:
+                data
+
+        return data + "\n"
+
+    def put(self, payload):
+        metric, metric_values = payload.split(" ", 1)
+        metric_values = metric_values.split(' ')
+        metric_values = metric_values[0], metric_values[1]
+        if metric not in self.data_dict:
+            self.data_dict[metric] = []
+            self.data_dict[metric].append(metric_values)
+        else:
+            self.data_dict[metric].append(metric_values)
+
+        return self.data_dict
+
+run_server("127.0.0.1", 8888)
