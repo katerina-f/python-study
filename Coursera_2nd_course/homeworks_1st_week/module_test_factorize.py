625c6ac44c99307b46983a77d664280a4b3788c1
Katerina <user@MacBook-Pro-User.local>
2019-03-09 22:24:26 +0300

    last commit. finished work.

--- Coursera_2nd_course/homeworks_1st_week/module_test_factorize.py
+++ Coursera_2nd_course/homeworks_1st_week/module_test_factorize.py
@@ -10,44 +10,45 @@ def factorize(x):
 class TestFactorize(unittest.TestCase):
 
     def test_wrong_types_raise_exception(self):
-        self.cases = ("string", 1.5)
-        for x in self.cases:
-            with self.subTest(case=x):
-                self.assertRaises(TypeError, factorize, x)
+        cases = ("string", 1.5)
+        for case in cases:
+            with self.subTest(x=case):
+                self.assertRaises(TypeError, factorize, case)
 
     def test_negative(self):
-        self.cases = (-1, -10, -100)
-        for x in self.cases:
-            with self.subTest(case=x):
-                self.assertRaises(ValueError, factorize, x)
+        cases = (-1, -10, -100)
+        for case in cases:
+            with self.subTest(x=case):
+                self.assertRaises(ValueError, factorize, case)
 
     def test_zero_and_one_cases(self):
-        self.cases = (0, 1)
-        for x in self.cases:
-            with self.subTest(case=x):
-                a = (x,)
-                self.assertEqual(a, factorize(x))
+        cases = [0, 1]
+        result = [(0,), (1,)]
+        for val,res in zip(cases,result):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), res)
 
     def test_simple_numbers(self):
-        self.cases = (3, 13, 29)
-        for x in self.cases:
-            with self.subTest(case=x):
-                a = (x,)
-                self.assertEqual(a, factorize(x))
+        cases = [3, 13, 29]
+        result = [(3,), (13,), (29,)]
+        for val,res in zip(cases,result):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), res)
 
     def test_two_simple_multipliers(self):
-        self.cases = {6: (2,3), 26: (2,13), 121: (11,11)}
-        for x in self.cases:
-            with self.subTest(case=x):
-                a = self.cases[x]
-                self.assertEqual(a, factorize(x))
+         cases = [6, 26, 121]
+         result = [(2,3), (2,13), (11,11)]
+         for val,res in zip(cases,result):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), res)
 
     def test_many_multipliers(self):
-        self.cases = {1001: (7,11,13), 9699690: (2,3,5,7,11,13,17,19)}
-        for x in self.cases:
-            with self.subTest(case=x):
-                a = self.cases[x]
-                self.assertEqual(a, factorize(x))
+        '''Что для чисел 1001 и 9699690 возвращаются соответственно кортежи (7, 11, 13) и (2, 3, 5, 7, 11, 13, 17, 19).'''
+        cases = [1001, 9699690]
+        result = [(7, 11, 13), (2, 3, 5, 7, 11, 13, 17, 19)]
+        for val, res in zip(cases, result):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), res)
 
 if True:
     unittest.main()
