9515d21d7e7ad79b2e1d73908335c16535b3919d
Katerina <user@MacBook-Pro-User.local>
2019-03-10 13:43:41 +0300

    10.03.2019

--- Coursera_2nd_course/homeworks_1st_week/test_for_sorting.py
+++ Coursera_2nd_course/homeworks_1st_week/test_for_sorting.py
@@ -79,8 +79,19 @@ def test_sort_algorithm_scalability(max_scale=100):
 
 
 def sort_algorithm(A):
-    "Sorting of list A on place."
-    pass
+    """
+    Sorting of list on place. Using Bubble Sort algorithm.
+    """
+    N = len(A)
+    list_is_sorted = False
+    bypass = 1
+    while not list_is_sorted:
+        list_is_sorted = True
+        for k in range(N - bypass):
+            if A[k] > A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
+                list_is_sorted = False
+        bypass += 1
 
 
 test_sort()
