b6684a628c1007e507366b8b38c530c80f7143e6
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-04-17 11:28:40 +0300

    complete parser for wiki

--- Coursera_3rd_course/homeworks_2nd_week/soup_sample/wikistat.py
+++ Coursera_3rd_course/homeworks_2nd_week/soup_sample/wikistat.py
@@ -43,7 +43,59 @@ def build_bridge(start, end, path):
         else:
             bridge.append(file)
 
-    return bridge
+    return list(reversed(bridge))
+
+
+def get_count_imgs(body):
+    imgs = body.findAll('img')
+    widths = []
+    for img in imgs:
+        try:
+            img = int(img['width'])
+            if img > 199:
+                widths.append(img)
+        except KeyError:
+            pass
+    return len(widths)
+
+
+def get_count_headers(body):
+    headers = body.find_all(re.compile('^h[1-6]$'))
+    count = 0
+    for header in headers:
+        text = header.text
+        if text.startswith('E') or text.startswith('C') or text.startswith('T'):
+            count += 1
+
+    return count
+
+
+def get_links_len(body):
+    linkslen = 0
+    for a in body.find_all('a'):
+        a_siblings = a.find_next_siblings()
+        len_arr = 1
+        for sib in a_siblings:
+            if sib.name == 'a':
+                len_arr+=1
+            else:
+                break
+        if len_arr > linkslen:
+            linkslen = len_arr
+
+    return linkslen
+
+
+def get_count_lists(body):
+    lists = 0
+    for ul in body.find_all('ul'):
+        if ul.parent.name == 'div' or ul.parent.name == "td":
+            lists += 1
+    for ol in body.find_all('ol'):
+        if ol.parent.name == 'div' or ul.parent.name == "td":
+            lists += 1
+
+    return lists
 
 def parse(start, end, path):
     """
@@ -53,25 +105,20 @@ def parse(start, end, path):
     Чтобы получить максимальный балл, придется искать все страницы. Удачи!
     """
 
-    bridge = list(reversed(build_bridge(start, end, path)))
+    bridge = build_bridge(start, end, path)
     # Искать список страниц можно как угодно, даже так: bridge = [end, start]
-
     # Когда есть список страниц, из них нужно вытащить данные и вернуть их
     out = {}
     for file in bridge:
         with open("{}{}".format(path, file)) as data:
             soup = BeautifulSoup(data, "lxml")
-
         body = soup.find(id="bodyContent")
-
         # TODO посчитать реальные значения
-        imgs = 5  # Количество картинок (img) с шириной (width) не меньше 200
-        headers = 10  # Количество заголовков, первая буква текста внутри которого: E, T или C
-        linkslen = 15  # Длина максимальной последовательности ссылок, между которыми нет других тегов
-        lists = 20  # Количество списков, не вложенных в другие списки
+
+        imgs = get_count_imgs(body)  # Количество картинок (img) с шириной (width) не меньше 200
+        headers = get_count_headers(body)  # Количество заголовков, первая буква текста внутри которого: E, T или C
+        linkslen = get_links_len(body)  # Длина максимальной последовательности ссылок, между которыми нет других тегов
+        lists = get_count_lists(body)  # Количество списков, не вложенных в другие списки
 
         out[file] = [imgs, headers, linkslen, lists]
-
     return out
-
-parse('Stone_Age', 'Python_(programming_language)', './wiki/')
