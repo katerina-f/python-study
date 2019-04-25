f0b9b455db4154b8e15a2125ec6605db0b3bae0d
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-04-25 13:07:54 +0300

    project simple examples from MySQL

---
+++ Coursera_3rd_course/homeworks_3rd_week/mysql_sample/results.sql
@@ -0,0 +1,32 @@
+use test
+set names utf8;
+
+-- 1. Выбрать все товары (все поля)
+select * from product
+
+-- 2. Выбрать названия всех автоматизированных складов
+select name from store where is_automated = 1
+
+-- 3. Посчитать общую сумму в деньгах всех продаж
+select sum(total) from sale
+
+-- 4. Получить уникальные store_id всех складов, с которых была хоть одна продажа
+select distinct store_id from sale
+
+-- 5. Получить уникальные store_id всех складов, с которых не было ни одной продажи
+select store_id from store natural left join sale where total is null
+
+-- 6. Получить для каждого товара название и среднюю стоимость единицы товара avg(total/quantity), если товар не продавался, он не попадает в отчет.
+select name, avg(total/quantity) from product as p join sale using(product_id) group by p.name order by 2
+
+-- 7. Получить названия всех продуктов, которые продавались только с единственного склада
+select name from product natural join sale group by name having count(distinct store_id)=1
+
+-- 8. Получить названия всех складов, с которых продавался только один продукт
+select name from sale natural join store group by name having count(distinct product_id) = 1;
+
+-- 9. Выберите все ряды (все поля) из продаж, в которых сумма продажи (total) максимальна (равна максимальной из всех встречающихся)
+select * from sale where total = (select max(total) from sale)
+
+-- 10. Выведите дату самых максимальных продаж, если таких дат несколько, то самую раннюю из них
+select date from sale group by date order by sum(total) DESC limit 1
