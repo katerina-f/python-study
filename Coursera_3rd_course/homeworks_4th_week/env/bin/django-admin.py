d8c54ad056ca165045d3c2c8075e7cc14cd3dab2
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-04-30 14:45:23 +0300

    finished 1st django project

---
+++ Coursera_3rd_course/homeworks_4th_week/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Katerina/Documents/python-study/Coursera_3rd_course/homeworks_4th_week/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
